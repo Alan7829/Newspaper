e617b2d443f008320c23a85033a8b27358f886eb
Alan Moreno <alanalexismartinez@gmail.com>
2021-06-18 17:49:36 -0500

    Change route for fonts

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -12,13 +12,13 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href={{asset("vendor/fontawesome-free/css/all.min.css")}} rel="stylesheet" type="text/css">
+    <link href={{ asset('vendor/fontawesome-free/css/all.min.css') }} rel="stylesheet" type="text/css">
     <link
-        href={{asset("https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i")}}
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href={{asset('css/sb-admin-2.min.css')}} rel="stylesheet">
+    <link href={{ asset('css/sb-admin-2.min.css') }} rel="stylesheet">
 
 </head>
 
@@ -183,8 +183,7 @@
                                 </h6>
                                 <a class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                                            alt="...">
+                                        <img class="rounded-circle" src="img/undraw_profile_1.svg" alt="...">
                                         <div class="status-indicator bg-success"></div>
                                     </div>
                                     <div class="font-weight-bold">
@@ -204,8 +203,7 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -284,21 +282,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src={{asset("vendor/jquery/jquery.min.js")}}></script>
-    <script src={{asset("vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+    <script src={{ asset('vendor/jquery/jquery.min.js') }}></script>
+    <script src={{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}></script>
 
     <!-- Core plugin JavaScript-->
-    <script src={{asset("vendor/jquery-easing/jquery.easing.min.js")}}></script>
+    <script src={{ asset('vendor/jquery-easing/jquery.easing.min.js') }}></script>
 
     <!-- Custom scripts for all pages-->
-    <script src={{asset('js/sb-admin-2.min.js')}}></script>
+    <script src={{ asset('js/sb-admin-2.min.js') }}></script>
 
     <!-- Page level plugins -->
-    <script src={{asset("vendor/chart.js/Chart.min.js")}}></script>
+    <script src={{ asset('vendor/chart.js/Chart.min.js') }}></script>
 
     <!-- Page level custom scripts -->
-    <script src={{asset("js/demo/chart-area-demo.js")}}></script>
-    <script src={{asset("js/demo/chart-pie-demo.js")}}></script>
+    <script src={{ asset('js/demo/chart-area-demo.js') }}></script>
+    <script src={{ asset('js/demo/chart-pie-demo.js') }}></script>
 
 </body>
 
