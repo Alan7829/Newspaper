6bcba6362d71637a780a066396604c32dabfda11
Alan Moreno <alanalexismartinez@gmail.com>
2021-06-18 20:28:54 -0500

    Add properties in Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,50 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Comment
+ *
+ * @property $id
+ * @property $author
+ * @property $email
+ * @property $message
+ * @property $news_id
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property News $news
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Comment extends Model
 {
-    use HasFactory;
+    
+    static $rules = [
+		'author' => 'required',
+		'email' => 'required',
+		'message' => 'required',
+		'news_id' => 'required',
+    ];
 
-    protected $fillable = ['id', 'author', 'email', 'message', 'news_id', 'created_at', 'updated_at'];
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['author','email','message','news_id'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function news()
+    {
+        return $this->hasOne('App\Models\News', 'id', 'news_id');
+    }
+    
+
 }
