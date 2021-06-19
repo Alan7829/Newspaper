36c60f429284f2d80cf561d7e5da1686b645c4c0
Alan Moreno <alanalexismartinez@gmail.com>
2021-06-19 00:00:21 -0500

    Change the routes for the cruds Category and Comment

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,8 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::prefix('admin')->group(function () {
-    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('AdminPanel');
+    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
 
-    Route::resource('/categories', App\Http\Controllers\Admin\CategoryController::class,);
-
-    // Route::get('/categories', [App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('CategoriesIndex');
-    // Route::get('/categories/create', [App\Http\Controllers\Admin\CategoryController::class, 'create'])->name('CategoriesCreate');
-    // Route::get('/categories/show', [App\Http\Controllers\Admin\CategoryController::class, 'show'])->name('CategoriesShow');
-
-    Route::get('/comments', [App\Http\Controllers\Admin\CommentController::class, 'index'])->name('CommentsIndex');
-    Route::get('/comments/create', [App\Http\Controllers\Admin\CommentController::class, 'create'])->name('CommentsCreate');
+    Route::resource('/categories', App\Http\Controllers\Admin\CategoryController::class);
+    Route::resource('/comments', App\Http\Controllers\Admin\CommentController::class);
 });
