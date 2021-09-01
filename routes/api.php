238d1431f1ca21a8655b861fc79582f97a26485f
Alan Moreno <alanalexismartinez@gmail.com>
2021-09-01 07:20:37 -0500

    Add routes of the api for CommentController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('article/{article_id}/comments', [CommentController::class, 'getArticleComments']);
+Route::patch('comments/{comment_id}/ban', [CommentController::class, 'banComment']);
+Route::patch('comments/{comment_id}/unban', [CommentController::class, 'unbanComment']);
