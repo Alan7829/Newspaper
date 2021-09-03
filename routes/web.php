3c86a96cef72de2c3f7443c03f372769066160ce
Alan Moreno <alanalexismartinez@gmail.com>
2021-09-03 10:26:40 -0500

    Fix routes admin and public

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('site.index');
+/* Rutas del sitio publico */
+Route::get('/', [App\Http\Controllers\Site\PageController::class, 'home'])->name('site.index');
 
-Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/* Rutas para iniciar sesion, registro, etc, sin proteccion con prefijo /admin/ */
 Route::prefix('admin')->group(function () {
-    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
+    Auth::routes();
+});
 
+/* Rutas protegidas con login con prefijo /admin/ */
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
     Route::resource('/categories', App\Http\Controllers\Admin\CategoryController::class);
     Route::resource('/comments', App\Http\Controllers\Admin\CommentController::class);
     Route::resource('/news', App\Http\Controllers\Admin\NewsController::class);
