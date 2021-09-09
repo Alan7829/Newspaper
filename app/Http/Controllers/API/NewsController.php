5e8dc35ff52e9e9f196c4ef46c36fa71671dc4dd
Alan Moreno <alanalexismartinez@gmail.com>
2021-09-09 12:52:17 -0500

    Edit the consult for get Articles by category

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -11,7 +11,10 @@ class NewsController extends Controller
     //Get all Articles with conditions by category and pub_status == 1 (Publicado)
     public function getArticlesByCategory($category_id)
     {
-        return News::where('category_id', $category_id)->where('pub_status', 1)->orderBy('id', 'DESC')->take(6)->get();
-        // return News::where('category_id', $category_id)->get();
+        return News::with(['category'])
+                    ->where('category_id', $category_id)
+                    ->where('pub_status', 1)
+                    ->orderBy('id', 'DESC')
+                    ->get();
     }
 }
